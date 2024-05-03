a5d307f377b1055b26494bd4d6c35c8379a46daf
Federico Schmitt <kolo.schmitt@gmail.com>
2024-05-03 02:10:54 -0300

    Refactor Context

--- frontend/src/context/PokemonContext.tsx
+++ frontend/src/context/PokemonContext.tsx
@@ -1,3 +1,25 @@
-import { createContext } from 'react'
+import React, { createContext, useState } from 'react';
+import { Pokemon } from '../types/Pokemon';
 
-export const PokemonContext = createContext(null)
+type PokemonContextType = {
+  chosenPokemon: Pokemon | null;
+  choosePokemon: (pokemon: Pokemon) => void;
+};
+
+const PokemonContext = createContext<PokemonContextType | undefined>(undefined);
+
+const PokemonProvider = ({ children }: { children: React.ReactNode }) => {
+  const [chosenPokemon, setChosenPokemon] = useState<Pokemon | null>(null);
+
+  const choosePokemon = (pokemon: Pokemon) => {
+    setChosenPokemon(pokemon);
+  };
+
+  const value = { chosenPokemon, choosePokemon };
+
+  return (
+    <PokemonContext.Provider value={value}>{children}</PokemonContext.Provider>
+  );
+};
+
+export { PokemonContext, PokemonProvider };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { AppTheme } from './theme/AppTheme.tsx'
-import { PokemonContext } from './context/PokemonContext.tsx'
+import { PokemonProvider } from './context/PokemonContext.tsx'
 
 import App from './App.tsx'
 import './index.css'
@@ -10,9 +10,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppTheme>
-      <PokemonContext.Provider value={null}>
+      <PokemonProvider>
         <App />
-      </PokemonContext.Provider>
+      </PokemonProvider>
     </AppTheme>
   </React.StrictMode>,
 )
