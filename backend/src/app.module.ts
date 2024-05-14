b6ee4d0e125a54c4a39db2299a1e53051c827395
Federico Schmitt <kolo.schmitt@gmail.com>
2024-05-14 12:34:55 -0300

    Config typeorm Migrations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { PokemonBattleModule } from './pokemon-battle/pokemon-battle.module';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: './db.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     PokemonModule,
     PokemonBattleModule,
   ],
